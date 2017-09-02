427f4cb63c18a4f49323ea6f9093842ff8887418
newser <newser@163.com>
2017-09-02 13:47:07 +0800

    gsl test

--- cmake/make_config.sh
+++ cmake/make_config.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
 # generate cmake config input
-sed 's/^#undef *\(HAVE.*\)/#define \1 @GSL_\1@/' ../gsl/config.h.in  > config.h.in
+sed -e 's/^#undef *\(HAVE.*\)/#define \1 @GSL_\1@/' \
+    -e 's/^#undef STDC_HEADERS/#define STDC_HEADERS @GSL_STDC_HEADERS@/' \
+    ../gsl/config.h.in  > config.h.in
 
 # generate cmake includes
+cfg_entry()
+{
+    if [ ! -f have/$1.cmake -o ! -s have/$1.cmake ]
+    then
+        echo need write have/$1.cmake
+        touch have/$1.cmake
+    fi
+
+    echo \# $1 >> config.cmake
+    echo include\(\${SOURCE_DIR}/cmake/have/$1.cmake\) >> config.cmake
+    echo >> config.cmake
+}
+
 echo \# gsl configuration\n > config.cmake
 echo >> config.cmake
 for f in $(sed -n 's/^#undef *\(HAVE.*\)/GSL_\1/p' ../gsl/config.h.in)
 do
-    if [ ! -f have/${f}.cmake -o ! -s have/${f}.cmake ]
-    then
-        echo need write have/${f}.cmake
-        touch have/${f}.cmake
-    fi
-
-    echo \# ${f} >> config.cmake
-    echo include\(\${SOURCE_DIR}/cmake/have/${f}.cmake\) >> config.cmake
-    echo >> config.cmake
+    cfg_entry ${f}
 done
+
+cfg_entry GSL_STDC_HEADERS

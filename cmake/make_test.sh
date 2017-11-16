03d8866e347354e723246321663abb9d35c77d17
newser <newser@163.com>
2017-11-16 15:40:45 +0800

    test cases

---
+++ cmake/make_test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+ALL=$(grep '^ *SUBDIRS *= *' ../gsl/Makefile.am | sed 's/^ *SUBDIRS *= *//')
+
+echo \# gsl directories > test.cmake
+echo >> test.cmake
+
+dir_entry()
+{
+    if [ ! -f test/$1.cmake -o ! -s test/$1.cmake ]
+    then
+        F=$(grep '^ *test_SOURCES *= *' ../gsl/$1/Makefile.am | sed 's/^ *test_SOURCES *= *//')
+        SRC=
+        for f in ${F}
+        do
+            SRC="${SRC} gsl/$1/${f}"
+        done
+
+        echo test entry: $1
+        echo \# $1 test sources > test/$1.cmake
+        if [ ! -z "${SRC}" ]
+        then
+            echo add_gsl_test\($1 ${SRC}\) >> test/$1.cmake
+            echo >> test/$1.cmake
+        fi
+    fi
+
+    echo \# $1 >> test.cmake
+    echo include\(\${SOURCE_DIR}/cmake/test/$1.cmake\) >> test.cmake
+    echo >> test.cmake
+}
+
+for d in ${ALL}
+do
+    dir_entry ${d}
+done
--- cmake/test/vector.cmake
+++ cmake/test/vector.cmake
@@ -1,3 +1,3 @@
 # vector test sources
-add_gsl_test(vector gsl/vector/test.c gsl/vector/test_static.c)
+add_gsl_test(vector gsl/vector/test.c)
 
